62e4ca890c6c22b5f83749fbb730ed714c94e74e
Darek <dariuszgrados@gmail.com>
2023-03-23 22:25:37 +0100

    Override RankedGradeBook's CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,9 +1,5 @@
 ﻿using GradeBook.Enums;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GradeBook.GradeBooks
 {
@@ -12,6 +8,19 @@ namespace GradeBook.GradeBooks
 		public RankedGradeBook(string name) : base(name)
 		{
 			Type = GradeBookType.Ranked;
+		}
+
+		public override void CalculateStatistics()
+		{
+			if (Students.Count < 5)
+			{
+				Console.WriteLine("Ranked grading requires at least 5 students.");
+			}
+			else
+			{
+				base.CalculateStatistics();
+			}
+
 		}
 	}
 }
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,10 +1,4 @@
 ﻿using GradeBook.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GradeBook.GradeBooks
 {
@@ -16,3 +10,4 @@ namespace GradeBook.GradeBooks
 		}
 	}
 }
+
