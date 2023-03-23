a1f6ef047ab8c8aab9139edd45c6815c5b5dd51e
Darek <dariuszgrados@gmail.com>
2023-03-23 22:30:32 +0100

    Override RankedGradeBook's CalculateStudentStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,7 +12,7 @@ namespace GradeBook.GradeBooks
 
 		public override char GetLetterGrade(double averageGrade)
 		{
-			if (averageGrade >=80)
+			if (averageGrade >= 80)
 				return 'A';
 			else if (averageGrade >= 60)
 				return 'B';
@@ -34,7 +34,18 @@ namespace GradeBook.GradeBooks
 			{
 				base.CalculateStatistics();
 			}
+		}
 
+		public override void CalculateStudentStatistics(string name)
+		{
+			if (Students.Count < 5)
+			{
+				Console.WriteLine("Ranked grading requires at least 5 students.");
+			}
+			else
+			{
+				base.CalculateStudentStatistics(name);
+			}
 		}
 	}
 }
